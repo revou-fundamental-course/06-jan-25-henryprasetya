7b4b702fc1c5eb41b121a93d85f783ed07aeba83
Henry <prasetyahenry450@gmail.com>
2025-01-15 14:08:28 +0700

    Update-gambar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-/// Slideshow
 var slideIndex = 1;
-showDivs(slideIndex);
 
 function plusDivs(n) {
   showDivs((slideIndex += n));
@@ -10,38 +8,29 @@ function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("img-slideshow");
   if (n > x.length) {
-    slideIndex = 1
-}
-
-  if (n < 1) {
-    slideIndex = x.length
-}
-
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-}
-
-  x[slideIndex - 1].style.display = "block";  
-}
-
-setInterval(() =>{
-    plusDivs(1);
-}, 3000);
-
-
-/// Prompt nama user ketika pertama kali load halaman HTML
-function changeName() {
-  var name = prompt("Please enter your name:");
-  if (name != null && name != "") {
-    document.getElementById("name").innerText = name;
+    slideIndex = 1;
   }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
 }
 
 window.onload = function() {
-  var storedName = localStorage.getItem("userName");
-  if (storedName) {
-    document.getElementById("name").innerText = storedName;
-  } else {
-    changeName();
+  showDivs(slideIndex);
+  setInterval(() => {
+    plusDivs(1);
+  }, 3000);
+
+  /// Prompt nama user ketika pertama kali load halaman HTML
+  function changeName() {
+    var name = prompt("Please enter your name:");
+    if (name != null && name != "") {
+      document.getElementById("name").innerText = name;
+    }
   }
-}
+  changeName();
+};
